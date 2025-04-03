09b770c486c4039535e83ee7cd160be2f6f77e70
Marco Nikander <m4n1k.developer@gmail.com>
2025-04-03 22:22:13 +0200

    check parentheses

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,3 +1,27 @@
 export function interpret(expr) {
+    if (!check_parentheses(expr)) {
+        return "ERROR: invalid parentheses";
+    }
     return "..."
+}
+
+export function check_parentheses(text) {
+    let count = 0;
+    let open_before_close = true;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] == '(') {
+            count++;
+        }
+        else if (text[i] == ')') {
+            count--;
+        }
+        if (count < 0) {
+            open_before_close = false;
+        }
+    }
+
+    let balanced = (count == 0);
+
+    return open_before_close && balanced;
 }
---
+++ test/check_parentheses.test.js
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { check_parentheses } from '../src/interpreter.js'
+
+describe('check_parentheses', () => {
+
+  it('(', () => {
+    expect(check_parentheses('(')).toBe(false)
+  }); 
+
+  it(')', () => {
+    expect(check_parentheses(')')).toBe(false)
+  });
+
+  it('((', () => {
+    expect(check_parentheses('((')).toBe(false)
+  })
+
+  it('))', () => {
+    expect(check_parentheses('))')).toBe(false)
+  })
+
+  it(')(', () => {
+    expect(check_parentheses(')(')).toBe(false)
+  })
+
+  it('()', () => {
+    expect(check_parentheses('()')).toBe(true)
+  });
+
+});
