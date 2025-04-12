c30e05a941dc42772507e640ddb0cc38daaa7d47
Marco Nikander <m4n1k.developer@gmail.com>
2025-04-12 19:38:32 +0200

    define AST nodes

--- src/parser.ts
+++ src/parser.ts
@@ -3,3 +3,29 @@
 export interface Node {
     kind: string,
 }
+
+export interface NodeAtom extends Node {
+    kind: "ND_ATOM",
+    value: number | string, // TODO: add Booleans
+}
+
+export interface NodeCall extends Node {
+    kind: "ND_CALL",
+    procedure: Node,
+    arguments: Node[],
+}
+
+export interface NodeExpression extends Node {
+    kind: "ND_EXPRESSION",
+    value: NodeAtom | NodeCall,
+}
+
+export interface NodeIdentifier extends Node {
+    kind: "ND_IDENTIFIER",
+    value: string,
+}
+
+export interface ParseError extends Node {
+    kind: "ND_ERROR",
+    value: string,
+}
