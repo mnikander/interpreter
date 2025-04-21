34bc008d8582d41588c0743590276aff5ca310ae
Marco Nikander <m4n1k.developer@gmail.com>
2025-04-21 14:41:34 +0200

    formatting of a unit test

--- test/interpreter.test.ts
+++ test/interpreter.test.ts
@@ -2,63 +2,63 @@ import { describe, it, expect } from 'vitest'
 import { interpret } from '../src/interpret.js'
 
 describe('interpret', () => {
-  
-  it('integer', () => {
-    const result = interpret("5");
-    expect(result).toBe(5);
-  });
 
-  it('addition', () => {
-    const result = interpret("(+ 1 2)");
-    expect(result).toBe(3);
-  });
+    it('integer', () => {
+        const result = interpret("5");
+        expect(result).toBe(5);
+    });
 
-  it('invalid addition', () => {
-    const result = interpret("(+ 1 2 3)");
-    expect(result).toContain("ERROR");
-    expect(result).toContain("argument");
-  });
+    it('addition', () => {
+        const result = interpret("(+ 1 2)");
+        expect(result).toBe(3);
+    });
 
-  it('left-nested addition', () => {
-    const result = interpret("(+ (+ 1 2) 3)");
-    expect(result).toBe(6);
-  });
+    it('invalid addition', () => {
+        const result = interpret("(+ 1 2 3)");
+        expect(result).toContain("ERROR");
+        expect(result).toContain("argument");
+    });
 
-  it('right-nested addition', () => {
-    const result = interpret("(+ 1 (+ 2 3))");
-    expect(result).toBe(6);
-  });
+    it('left-nested addition', () => {
+        const result = interpret("(+ (+ 1 2) 3)");
+        expect(result).toBe(6);
+    });
 
-  it('boolean', () => {
-    const result = interpret("True");
-    expect(result).toBe(true);
-  });
+    it('right-nested addition', () => {
+        const result = interpret("(+ 1 (+ 2 3))");
+        expect(result).toBe(6);
+    });
 
-  it('and', () => {
-    const ff = interpret("(& False False)");
-    expect(ff).toBe(false);
+    it('boolean', () => {
+        const result = interpret("True");
+        expect(result).toBe(true);
+    });
 
-    const ft = interpret("(& False True)");
-    expect(ft).toBe(false);
+    it('and', () => {
+        const ff = interpret("(& False False)");
+        expect(ff).toBe(false);
 
-    const tf = interpret("(& True False)");
-    expect(tf).toBe(false);
+        const ft = interpret("(& False True)");
+        expect(ft).toBe(false);
 
-    const tt = interpret("(& True True)");
-    expect(tt).toBe(true);
-  });
+        const tf = interpret("(& True False)");
+        expect(tf).toBe(false);
 
-  it('or', () => {
-    const ff = interpret("(| False False)");
-    expect(ff).toBe(false);
+        const tt = interpret("(& True True)");
+        expect(tt).toBe(true);
+    });
 
-    const ft = interpret("(| False True)");
-    expect(ft).toBe(true);
+    it('or', () => {
+        const ff = interpret("(| False False)");
+        expect(ff).toBe(false);
 
-    const tf = interpret("(| True False)");
-    expect(tf).toBe(true);
+        const ft = interpret("(| False True)");
+        expect(ft).toBe(true);
 
-    const tt = interpret("(| True True)");
-    expect(tt).toBe(true);
-  });
+        const tf = interpret("(| True False)");
+        expect(tf).toBe(true);
+
+        const tt = interpret("(| True True)");
+        expect(tt).toBe(true);
+    });
 });
