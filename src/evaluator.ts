38c7c85a9d552469ae019a04374b6b09d1d42737
Marco Nikander <m4n1k.developer@gmail.com>
2025-04-29 11:53:22 +0200

    evaluate functions of any arity

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -8,23 +8,23 @@ export type Symbol =
     | { kind: "EV_VALUE",    value: boolean | number };
 
 const environment: Map<string, Symbol> = new Map<string, Symbol>([
-    ['+',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] + args[1]; }, about: "(+ 5 2)\t\taddition"}],
-    ['-',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] - args[1]; }, about: "(- 5 2)\t\tsubtraction"}],
-    ['*',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] * args[1]; }, about: "(* 5 2)\t\tmultiplication"}],
-    ['/',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] / args[1]; }, about: "(/ 5 2)\t\tdivision"}],
-    ['%',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] % args[1]; }, about: "(% 5 2)\t\tremainder after division"}],
-    ['<',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] < args[1]; }, about: "(< 5 2)\t\tless than"}],
-    ['>',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] > args[1]; }, about: "(> 5 2)\t\tgreater than"}],
-    ['<=',   {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] <= args[1]; }, about: "(<= 5 2)\tless than or equal to"}],
-    ['>=',   {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: number[] ) { return args[0] >= args[1]; }, about: "(>= 5 2)\tgreater than or equal to"}],
-    ['==',   {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: any[] ) { return args[0] == args[1]; }, about: "(== 5 2)\tequal to"}],
-    ['!=',   {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: any[] ) { return args[0] != args[1]; }, about: "(!= 5 2)\tunequal to"}],
-    ['&',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: boolean[] ) { return args[0] && args[1]; }, about: "(& True False)\tlogical and"}],
-    ['|',    {kind: "EV_FUNCTION", arity: 2, value: function ( ...args: boolean[] ) { return args[0] || args[1]; }, about: "(| True False)\tlogical or"}],
-    ['!',    {kind: "EV_FUNCTION", arity: 1, value: function ( ...args: boolean[] ) { return !args[0]; }, about: "(! True)\tlogical negation"}],
-    ['if',   {kind: "EV_FUNCTION", arity: 3, value: function ( ...args: any[] ) { return args[0] ? args[1] : args[2]; }, about: "(if True 4 8)\tif-expression"}],
-    ['help', {kind: "EV_FUNCTION", arity: 0, value: function ( ...args: any[] ) { return help(environment); }, about: "(help)\t\tprints this dialog"}],
-    ['Help', {kind: "EV_FUNCTION", arity: 0, value: function ( ...args: any[] ) { return help(environment); }, about: "(Help)\t\tprints this dialog"}],
+    ['+',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] + args[1]; }, about: "(+ 5 2)\t\taddition"}],
+    ['-',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] - args[1]; }, about: "(- 5 2)\t\tsubtraction"}],
+    ['*',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] * args[1]; }, about: "(* 5 2)\t\tmultiplication"}],
+    ['/',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] / args[1]; }, about: "(/ 5 2)\t\tdivision"}],
+    ['%',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] % args[1]; }, about: "(% 5 2)\t\tremainder after division"}],
+    ['<',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] < args[1]; }, about: "(< 5 2)\t\tless than"}],
+    ['>',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] > args[1]; }, about: "(> 5 2)\t\tgreater than"}],
+    ['<=',   {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] <= args[1]; }, about: "(<= 5 2)\tless than or equal to"}],
+    ['>=',   {kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] ) { return args[0] >= args[1]; }, about: "(>= 5 2)\tgreater than or equal to"}],
+    ['==',   {kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] ) { return args[0] == args[1]; }, about: "(== 5 2)\tequal to"}],
+    ['!=',   {kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] ) { return args[0] != args[1]; }, about: "(!= 5 2)\tunequal to"}],
+    ['&',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] && args[1]; }, about: "(& True False)\tlogical and"}],
+    ['|',    {kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] || args[1]; }, about: "(| True False)\tlogical or"}],
+    ['!',    {kind: "EV_FUNCTION", arity: 1, value: function ( args: boolean[] ) { return !args[0]; }, about: "(! True)\tlogical negation"}],
+    ['if',   {kind: "EV_FUNCTION", arity: 3, value: function ( args: any[] ) { return args[0] ? args[1] : args[2]; }, about: "(if True 4 8)\tif-expression"}],
+    ['help', {kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] ) { return help(environment); }, about: "(help)\t\tprints this dialog"}],
+    ['Help', {kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] ) { return help(environment); }, about: "(Help)\t\tprints this dialog"}],
 ]);
 
 function help(environment: Map<string, Symbol>): string {
@@ -69,66 +69,15 @@ export function evaluate(ast: ASTNode): Error | Symbol {
         const entry: Error | Symbol = evaluate(id);
         if(entry?.kind === "EV_FUNCTION") {
             if(entry.arity === call.params.length) {
-                if(entry.arity == 0) {
-                    const f = entry.value as Function;
-                    return {kind: "EV_VALUE", value: f()};
-                }
-                if(entry.arity == 1) {
-                    const left = evaluate(call.params[0]);
-                    if (left.kind === "EV_VALUE") {
-                        const f = entry.value as Function;
-                        const l = left.value;
-                        return {kind: "EV_VALUE", value: f(l)};
-                    }
-                    else {
-                        return left;
-                    }
-                }
-                if(entry.arity == 2) {
-                    const left  = evaluate(call.params[0]);
-                    const right = evaluate(call.params[1]);
-                    if (left.kind === "EV_VALUE") {
-                        if (right.kind === "EV_VALUE") {
-                        const f = entry.value as Function;
-                        const l = left.value;
-                        const r = right.value;
-                        return {kind: "EV_VALUE", value: f(l, r)};
-                        }
-                        else {
-                            return right;
-                        }
-                    }
-                    else {
-                        return left;
-                    }
-                }
-                if(entry.arity == 3) {
-                    const zero = evaluate(call.params[0]);
-                    const one  = evaluate(call.params[1]);
-                    const two  = evaluate(call.params[2]);
-                    if (zero.kind === "EV_VALUE") {
-                        if (one.kind === "EV_VALUE") {
-                            if (two.kind === "EV_VALUE") {
-                                const f = entry.value as Function;
-                                const z = zero.value;
-                                const o = one.value;
-                                const t = two.value;
-                                return {kind: "EV_VALUE", value: f(z, o, t)};
-                            }
-                            else {
-                                return two;
-                            }
-                        }
-                        else {
-                            return one;
-                        }
-                    }
-                    else {
-                        return zero;
-                    }
+                const ev_args: (Error | Symbol)[] = call.params.map(evaluate);
+                const ok: boolean                 = (ev_args.find(is_error) === undefined);
+                if(ok) {
+                    const fn   = entry.value as Function;
+                    const args = (ev_args as Symbol[]).map((s: Symbol) => { return s.value; });
+                    return {kind: "EV_VALUE", value: fn(args)};
                 }
                 else {
-                    return {kind: "Evaluation Error", message: `Only functions with 0, 1, 2, or 3 arguments can be evaluated, but ${call.params.length} argument(s) were provided`};
+                    return { kind: "Evaluation Error", message: `could not evaluate function arguments`};
                 }
             }
             else {
