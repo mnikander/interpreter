49faf631503e1e6cb565f28f30f001e897993a40
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-04 09:23:54 +0200

    refactor environment to support nesting

--- src/environment.ts
+++ src/environment.ts
@@ -6,35 +6,52 @@ export type Symbol =
     | { kind: "EV_FUNCTION", value: boolean | number | ((...args: any[]) => any), arity: number, about?: string, }
     | { kind: "EV_VALUE",    value: boolean | number };
 
-export function lookup(identifier: ASTAtom, builtin: Map<string, Symbol>): undefined | Symbol {
-    return builtin.get(String(identifier.value));
+export function lookup(identifier: ASTAtom, environment: Environment): undefined | Symbol {
+    const entry: undefined | Symbol = environment.symbols.get(String(identifier.value));
+    if (entry !== undefined) {
+        return entry;
+    }
+    else {
+        if (environment.parent !== undefined) {
+            return lookup(identifier, environment.parent);
+        }
+        else {
+            // we are already in the global context (the root node), and have nowhere left to search for the symbol
+            return undefined;
+        }
+    }
 }
 
-export type Environment = Map<string, Symbol>;
+export type Environment = {
+    parent: undefined | Environment,
+    symbols: Map<string, Symbol>,
+};
 
-export const builtin: Environment = new Map<string, Symbol>([
-]);
-    ['+',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] + args[1]; },  about: "(+ 5 2)\t\taddition" }],
-    ['-',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] - args[1]; },  about: "(- 5 2)\t\tsubtraction" }],
-    ['*',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] * args[1]; },  about: "(* 5 2)\t\tmultiplication" }],
-    ['/',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] / args[1]; },  about: "(/ 5 2)\t\tdivision" }],
-    ['%',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] % args[1]; },  about: "(% 5 2)\t\tremainder after division" }],
-    ['<',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] < args[1]; },  about: "(< 5 2)\t\tless than" }],
-    ['>',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] > args[1]; },  about: "(> 5 2)\t\tgreater than" }],
-    ['<=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] <= args[1]; }, about: "(<= 5 2)\tless than or equal to" }],
-    ['>=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] >= args[1]; }, about: "(>= 5 2)\tgreater than or equal to" }],
-    ['==',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] )     { return args[0] == args[1]; }, about: "(== 5 2)\tequal to" }],
-    ['!=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] )     { return args[0] != args[1]; }, about: "(!= 5 2)\tunequal to" }],
-    ['&',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] && args[1]; }, about: "(& True False)\tlogical and" }],
-    ['|',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] || args[1]; }, about: "(| True False)\tlogical or" }],
-    ['!',    { kind: "EV_FUNCTION", arity: 1, value: function ( args: boolean[] ) { return !args[0]; },           about: "(! True)\tlogical negation" }],
-    ['if',   { kind: "EV_FUNCTION", arity: 3, value: function ( args: any[] )     { return args[0] ? args[1] : args[2]; }, about: "(if True 4 8)\tif-expression" }],
-    ['help', { kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] )     { return help(); },             about: "(help)\t\tprints this dialog" }],
-    ['Help', { kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] )     { return help(); },             about: "(Help)\t\tprints this dialog" }],
+export const builtin: Environment = {
+    parent: undefined,
+    symbols: new Map<string, Symbol>([
+        ['+',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] + args[1]; },  about: "(+ 5 2)\t\taddition" }],
+        ['-',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] - args[1]; },  about: "(- 5 2)\t\tsubtraction" }],
+        ['*',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] * args[1]; },  about: "(* 5 2)\t\tmultiplication" }],
+        ['/',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] / args[1]; },  about: "(/ 5 2)\t\tdivision" }],
+        ['%',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] % args[1]; },  about: "(% 5 2)\t\tremainder after division" }],
+        ['<',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] < args[1]; },  about: "(< 5 2)\t\tless than" }],
+        ['>',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] > args[1]; },  about: "(> 5 2)\t\tgreater than" }],
+        ['<=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] <= args[1]; }, about: "(<= 5 2)\tless than or equal to" }],
+        ['>=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: number[] )  { return args[0] >= args[1]; }, about: "(>= 5 2)\tgreater than or equal to" }],
+        ['==',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] )     { return args[0] == args[1]; }, about: "(== 5 2)\tequal to" }],
+        ['!=',   { kind: "EV_FUNCTION", arity: 2, value: function ( args: any[] )     { return args[0] != args[1]; }, about: "(!= 5 2)\tunequal to" }],
+        ['&',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] && args[1]; }, about: "(& True False)\tlogical and" }],
+        ['|',    { kind: "EV_FUNCTION", arity: 2, value: function ( args: boolean[] ) { return args[0] || args[1]; }, about: "(| True False)\tlogical or" }],
+        ['!',    { kind: "EV_FUNCTION", arity: 1, value: function ( args: boolean[] ) { return !args[0]; },           about: "(! True)\tlogical negation" }],
+        ['if',   { kind: "EV_FUNCTION", arity: 3, value: function ( args: any[] )     { return args[0] ? args[1] : args[2]; }, about: "(if True 4 8)\tif-expression" }],
+        ['help', { kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] )     { return help(); },             about: "(help)\t\tprints this dialog" }],
+        ['Help', { kind: "EV_FUNCTION", arity: 0, value: function ( args: any[] )     { return help(); },             about: "(Help)\t\tprints this dialog" }],
+])};
 
 function help(): string {
     let message: string = "\nSymbol\tUsage\t\tName\n------------------------------------------------\n";
-    for (const [key, value] of builtin.entries()) {
+    for (const [key, value] of builtin.symbols.entries()) {
         if ( value.kind === "EV_FUNCTION" && value.arity !== undefined) {
             message += `${key}\t${value?.about}\n`;
         }
