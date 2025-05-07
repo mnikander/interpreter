d11eacb79406bc57d38872ec33e72d90d9d5d626
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-07 20:46:59 +0200

    proto: stub 'parse_atom'

--- proto/parser.ts
+++ proto/parser.ts
@@ -1,16 +1,21 @@
 // Copyright (c) 2025 Marco Nikander
 
-import { AST } from "./ast";
+import { AST, Leaf } from "./ast";
 import { Result } from "./error";
 import { Token } from "./token";
 
 
 // line = expr *space
 export function parse_line(tokens: Token[]): Result<AST> {
-    return { ok: false, error: { kind: "Error", subkind: "Parsing", token_id: 0, message: "unknown error"} };
+    return { ok: false, error: { kind: "Error", subkind: "Parsing", token_id: 0, message: "a line"} };
 }
 
 // expr = *space (atom / (open *space expr *(space *space expr)))
 export function parse_expr(tokens: Token[]): Result<AST> {
-    return { ok: false, error: { kind: "Error", subkind: "Parsing", token_id: 0, message: "unknown error"} };
+    return { ok: false, error: { kind: "Error", subkind: "Parsing", token_id: 0, message: "an expression"} };
+}
+
+// atom = boolean / number / string / identifier
+export function parse_atom(tokens: Token[]): Result<Leaf> {
+    return { ok: false, error: { kind: "Error", subkind: "Parsing", token_id: 0, message: "an atom"} };
 }
