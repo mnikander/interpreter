8eacc8141bf41eb289003bf17d63be4399df0a83
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-07 12:40:37 +0200

    proto: remove the 'Ok' type

--- proto/error.ts
+++ proto/error.ts
@@ -2,20 +2,11 @@
 
 import { Item } from "./item";
 
-export interface Ok extends Item {
-    kind: "Ok",
-    subkind: "Ok",
-}
-
 export interface Error extends Item {
     kind: "Error",
     subkind: "Lexing" | "Parsing" | "Semantic" | "Evaluation",
     token_id: number,
     message: string,
-}
-
-export function is_ok(item: Item): item is Ok {
-    return item.kind === "Ok";
 }
 
 export function is_error(item: Item | Item[]): item is Error {
