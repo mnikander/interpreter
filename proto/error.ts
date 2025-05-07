d7a5ba5246e0fd45965065db23ae92dbeadb3a17
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-07 10:55:36 +0200

    proto: allow the is_error function to handle arrays so it can be used more generally

--- proto/error.ts
+++ proto/error.ts
@@ -18,6 +18,12 @@ export function is_ok(item: Item): item is Ok {
     return item.kind === "Ok";
 }
 
-export function is_error(item: Item): item is Error {
-    return item.kind === "Error";
+export function is_error(item: Item | Item[]): item is Error {
+    if (Array.isArray(item)) {
+        const first_error = item.find(is_error);
+        return first_error !== undefined;
+    }
+    else {
+        return item.kind === "Error";
+    }
 }
