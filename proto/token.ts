49434e82af0c372d7d683cfdc0cbe62ab893c3f6
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-07 10:54:52 +0200

    proto: differentiate tokens

--- proto/token.ts
+++ proto/token.ts
@@ -1,12 +1,13 @@
 // Copyright (c) 2025 Marco Nikander
 
 import { Item } from "./item";
+import { Error } from "./error";
 
 export interface Token extends Item {
     kind: "Token",
     subkind: string,
     id: number,
-    value: any,
+    value: boolean | number | string | "(" | ")",
 }
 
 export interface TokenOpen extends Token {
@@ -23,12 +24,63 @@ export interface TokenClose extends Token {
     value: ")",
 }
 
-export interface TokenAtom extends Token {
+export interface TokenBoolean extends Token {
     kind: "Token",
-    subkind: "Atom",
+    subkind: "Boolean",
     id: number,
-    value: boolean | number | string,
+    value: boolean,
 }
+
+export interface TokenNumber extends Token {
+    kind: "Token",
+    subkind: "Number",
+    id: number,
+    value: number,
+}
+
+export interface TokenString extends Token {
+    kind: "Token",
+    subkind: "String",
+    id: number,
+    value: string,
+}
+
+export interface TokenIdentifier extends Token {
+    kind: "Token",
+    subkind: "Identifier",
+    id: number,
+    value: string,
+}
+
+export function make_token_open(id: number, word: string): TokenOpen {
+    return { kind: "Token", subkind: "Open", id: id, value: "(" };
+}
+
+export function make_token_close(id: number, word: string): TokenClose {
+    return { kind: "Token", subkind: "Close", id: id, value: ")" };
+}
+
+export function make_token_boolean(id: number, word: "true" | "True" | "false" | "False"): TokenBoolean {
+    if (word === "false" || word === "False") {
+        return { kind: "Token", subkind: "Boolean", id: id, value: false };
+    }
+    else {
+        return { kind: "Token", subkind: "Boolean", id: id, value: true };
+    }
+}
+
+export function make_token_number(id: number, word: string): TokenNumber {
+    return { kind: "Token", subkind: "Number", id: id, value: Number(word) };
+}
+
+export function make_token_string(id: number, word: string): TokenString {
+    return { kind: "Token", subkind: "String", id: id, value: word };
+}
+
+export function make_token_identifier(id: number, word: string): TokenIdentifier {
+    return { kind: "Token", subkind: "Identifier", id: id, value: word };
+}
+
 
 // type predicates
 
@@ -44,6 +96,18 @@ export function is_token_close(item: Item): item is TokenClose {
     return is_token(item) && item.subkind === "Close";
 }
 
-export function is_token_atom(item: Item): item is TokenAtom {
-    return is_token(item) && item.subkind === "Atom";
+export function is_token_boolean(item: Item): item is TokenBoolean {
+    return is_token(item) && item.subkind === "Boolean";
+}
+
+export function is_token_number(item: Item): item is TokenNumber {
+    return is_token(item) && item.subkind === "Number";
+}
+
+export function is_token_string(item: Item): item is TokenString {
+    return is_token(item) && item.subkind === "String";
+}
+
+export function is_token_identifier(item: Item): item is TokenIdentifier {
+    return is_token(item) && item.subkind === "Identifier";
 }
