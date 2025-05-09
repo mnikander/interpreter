eebbc12ba60b2bd8c00777d2e304c8c7b047d028
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-09 12:42:06 +0200

    proto: re-organize the contents of ast.ts

--- proto/ast.ts
+++ proto/ast.ts
@@ -3,17 +3,10 @@
 import { Item } from "./item";
 import { Token } from "./token";
 
-// general types for the AST
 
 export type AST = Leaf | Node;
 
-export interface Leaf extends Item {
-    kind: string,
-    subkind: string,
-    token_id: number,
-    node_id: number,
-    value: boolean | number | string,
-}
+// node types for the AST
 
 export interface Node extends Item {
     kind: string,
@@ -23,40 +16,12 @@ export interface Node extends Item {
     data: AST[]
 };
 
-// specific types for the AST
-
-export interface LeafBoolean    extends Leaf { kind: "Leaf", subkind: "Boolean",    token_id: number, node_id: number, value: boolean };
-export interface LeafNumber     extends Leaf { kind: "Leaf", subkind: "Number",     token_id: number, node_id: number, value: number };
-export interface LeafString     extends Leaf { kind: "Leaf", subkind: "String",     token_id: number, node_id: number, value: string };
-export interface LeafIdentifier extends Leaf { kind: "Leaf", subkind: "Identifier", token_id: number, node_id: number, value: string };
-export interface LeafReference  extends Leaf { kind: "Leaf", subkind: "Reference",  token_id: number, node_id: number, value: string }; // I could switch to an ID number later
-
-export interface NodeCall extends Node {
-    kind: "Node",
-    subkind: "Call",
-    token_id: number,
-    node_id: number,
-    data: AST[]
-}
-
-export interface NodeLet extends Node {
-    kind: "Node",
-    subkind: "Let",
-    token_id: number,
-    node_id: number,
-    data: [AST, AST, AST, AST]
-}
-
-// type predicates
-
-export function is_leaf(item: Item): item is Leaf { return item.kind === "Leaf"; }
-export function is_leaf_boolean(item: Item): item is LeafBoolean { return is_leaf(item) && item.subkind === "Boolean"; }
-export function is_leaf_number(item: Item): item is LeafNumber { return is_leaf(item) && item.subkind === "Number"; }
-export function is_leaf_string(item: Item): item is LeafString { return is_leaf(item) && item.subkind === "String"; }
-export function is_leaf_identifier(item: Item): item is LeafIdentifier { return is_leaf(item) && item.subkind === "Identifier"; }
-export function is_leaf_reference(item: Item): item is LeafReference { return is_leaf(item) && item.subkind === "Reference"; }
+export interface NodeCall extends Node { kind: "Node", subkind: "Call", token_id: number, node_id: number, data: AST[] }
+export interface NodeLet  extends Node { kind: "Node", subkind: "Let", token_id: number, node_id: number, data: [AST, AST, AST, AST] }
 
 export function is_node(item: Item): item is Node { return item.kind === "Node"; }
+
+// type predicates for nodes
 
 export function is_node_call(item: Item): item is NodeCall {
     return is_node(item)
@@ -72,6 +37,33 @@ export function is_node_let(item: Item): item is NodeLet {
     && item.data[0].value === 'let'
     && is_leaf_identifier(item.data[1]);
 }
+
+// leaf types for the AST
+
+export interface Leaf extends Item {
+    kind: string,
+    subkind: string,
+    token_id: number,
+    node_id: number,
+    value: boolean | number | string,
+}
+
+export interface LeafBoolean    extends Leaf { kind: "Leaf", subkind: "Boolean",    token_id: number, node_id: number, value: boolean };
+export interface LeafNumber     extends Leaf { kind: "Leaf", subkind: "Number",     token_id: number, node_id: number, value: number };
+export interface LeafString     extends Leaf { kind: "Leaf", subkind: "String",     token_id: number, node_id: number, value: string };
+export interface LeafIdentifier extends Leaf { kind: "Leaf", subkind: "Identifier", token_id: number, node_id: number, value: string };
+export interface LeafReference  extends Leaf { kind: "Leaf", subkind: "Reference",  token_id: number, node_id: number, value: string }; // I could switch to an ID number later
+
+// type predicates for leaves
+
+export function is_leaf(item: Item): item is Leaf { return item.kind === "Leaf"; }
+export function is_leaf_boolean(item: Item): item is LeafBoolean { return is_leaf(item) && item.subkind === "Boolean"; }
+export function is_leaf_number(item: Item): item is LeafNumber { return is_leaf(item) && item.subkind === "Number"; }
+export function is_leaf_string(item: Item): item is LeafString { return is_leaf(item) && item.subkind === "String"; }
+export function is_leaf_identifier(item: Item): item is LeafIdentifier { return is_leaf(item) && item.subkind === "Identifier"; }
+export function is_leaf_reference(item: Item): item is LeafReference { return is_leaf(item) && item.subkind === "Reference"; }
+
+// constructors for leaves
 
 export function make_leaf(index: number, node_counter: number, token: Token): Leaf {
     return {kind: "Leaf", subkind: token.subkind, token_id: token.id, node_id: node_counter, value: token.value}
