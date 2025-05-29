09ef0d837b84c5a0cc4cda0d3fa25a39933b8f67
Marco Nikander <m4n1k.developer@gmail.com>
2025-05-29 21:30:30 +0200

    proto: handle the trivial cases in identifier check

--- proto/check_identifiers.ts
+++ proto/check_identifiers.ts
@@ -1,9 +1,25 @@
 // Copyright (c) 2025 Marco Nikander
 
-import { AST } from "./ast";
+import { AST, is_leaf, is_leaf_boolean, is_leaf_identifier, is_leaf_number, is_leaf_string, is_node } from "./ast";
 import { ValueEnvironment } from "./evaluator";
 import { error, Result } from "./error";
 
 export function check_identifiers(ast: AST, env: ValueEnvironment): Result<undefined> {
-    return { ok: true, value: undefined };
+    if (is_leaf_boolean(ast) || is_leaf_number(ast) || is_leaf_string(ast)) {
+        return { ok: true, value: undefined};
+    }
+    else if (is_leaf_identifier(ast)) {
+        // TODO: insert the actual identifier checking here
+        return { ok: true, value: undefined};
+    }
+    else if (is_node(ast)) {
+        for (let child of ast.data) {
+            const result = check_identifiers(child, env);
+            if (!result.ok) return { ok: false, error: result.error};
+        }
+        return { ok: true, value: undefined };
+    }
+    else {
+        return { ok: false, error: error("Analyzing", "unknown kind of AST entry", ast.token_id) };
+    }
 }
