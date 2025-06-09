48f8b206c7297f635797992b216988931d4f97a6
Marco Nikander <m4n1k.developer@gmail.com>
2025-06-09 12:19:45 +0200

    fine-grained structure for test cases of the prototype de Bruijn level evaluator

--- test/proto_de_bruijn_levels.test.ts
+++ test/proto_de_bruijn_levels.test.ts
@@ -1,16 +1,30 @@
 import { describe, it, expect } from 'vitest'
 import { debruijn, evaluate } from '../proto/de_bruijn_levels'
 
-describe('must convert binding names to de Bruijn levels', () => {
-    it('must substitute bound variables in a lambda function', () => {
-        expect(debruijn( ['lambda', 'a', 42]                  )).toStrictEqual(['lambda', 42]);
-        expect(debruijn( ['lambda', 'a', 'a']                 )).toStrictEqual(['lambda', { level: 0 }]);
-        expect(debruijn( ['lambda', 'a', ['lambda', 'b', 'a']])).toStrictEqual(['lambda', ['lambda', { level: 0 }]]);
+describe('when none of the arguments are provided, bound variable names must be substituted with de Bruijn levels', () => {
+    it('constant function', () => {
+        expect(debruijn( ['lambda', 'a', 42] )).toStrictEqual(['lambda', 42]);
     });
 
-    it('must substitute bound variables in a lambda function with provided arguments', () => {
-        expect(debruijn(  [['lambda', 'a', 42], 1]                      )).toStrictEqual( [['lambda', 42], 1]);
-        expect(debruijn(  [['lambda', 'a', 'a'], 1]                     )).toStrictEqual( [['lambda', { level: 0 }], 1]);
+    it('I combinator', () => {
+        expect(debruijn( ['lambda', 'a', 'a'] )).toStrictEqual(['lambda', { level: 0 }]);
+    });
+
+    it('K combinator', () => {
+        expect(debruijn( ['lambda', 'a', ['lambda', 'b', 'a']])).toStrictEqual(['lambda', ['lambda', { level: 0 }]]);
+    });
+});
+
+describe('when all of the arguments are provided, bound variable names must be substituted with de Bruijn levels', () => {
+    it('constant function', () => {
+        expect(debruijn(  [['lambda', 'a', 42], 1] )).toStrictEqual( [['lambda', 42], 1]);
+    });
+
+    it('I combinator', () => {
+        expect(debruijn(  [['lambda', 'a', 'a'], 1] )).toStrictEqual( [['lambda', { level: 0 }], 1]);
+    });
+
+    it('K combinator', () => {
         expect(debruijn( [[['lambda', 'a', ['lambda', 'b', 'a']], 1], 2])).toStrictEqual([[['lambda', ['lambda', { level: 0 }]], 1], 2]);
     });
 });
