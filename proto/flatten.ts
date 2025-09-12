cba017ca41ca1e88c19d9f300fd28fcbabcb67c0
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-12 19:35:13 +0200

    adapt flatten to use the AST from the prototype OO parser and update/cleanup tests

--- proto/flatten.ts
+++ proto/flatten.ts
@@ -1,42 +1,42 @@
 // Copyright (c) 2025 Marco Nikander
 
-import { AST, is_boolean, is_call, is_identifier, is_lambda, is_let, is_number, is_string } from "../src/ast";
-import { Value, Id, Flat_Constant, Flat_Identifier, Flat_Reference, Flat_Lambda, Flat_Let, Flat_Call, Flat_Plus, Flat_Minus, Flat_Node, Flat_Atom, Flat_AST } from "./flat_ast";
+import { Nested_Expression, is_boolean, is_call, is_identifier, is_lambda, is_let, is_number, is_string } from "./parser_oo";
+import { Value, Id, Flat_Constant, Flat_Identifier, Flat_Reference, Flat_Lambda, Flat_Let, Flat_Call, Flat_Node, Flat_Atom, Flat_AST } from "./flat_ast";
 
-export function flatten(ast: AST, node_count: number): Flat_AST {
+export function flatten(ast: Nested_Expression, node_count: number): Flat_AST {
     const flat: Flat_Node[] = Array(node_count);
     return flatten_nodes(ast, flat);
 }
 
-function flatten_nodes(nested_ast: AST, flat_ast: Flat_AST): Flat_AST {
+function flatten_nodes(nested_ast: Nested_Expression, flat_ast: Flat_AST): Flat_AST {
     if (is_lambda(nested_ast)) {
         const index               = nested_ast.id;
-        const binding_id          = { id: nested_ast.data[1].id };
-        const body_id             = { id: nested_ast.data[2].id };
+        const binding_id          = { id: nested_ast.binding.id };
+        const body_id             = { id: nested_ast.body.id };
         let node: Flat_Lambda     = { id: index, token: nested_ast.token, kind: 'Flat_Lambda', binding: binding_id, body: body_id };
         flat_ast[index]           = node;
-        flat_ast                  = flatten_nodes(nested_ast.data[1], flat_ast);
-        flat_ast                  = flatten_nodes(nested_ast.data[2], flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.binding, flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.body, flat_ast);
     }
     else if (is_let(nested_ast)) {
         const index               = nested_ast.id;
-        const binding_id          = { id: nested_ast.data[1].id };
-        const value_id            = { id: nested_ast.data[2].id };
-        const body_id             = { id: nested_ast.data[3].id };
+        const binding_id          = { id: nested_ast.binding.id };
+        const value_id            = { id: nested_ast.value.id };
+        const body_id             = { id: nested_ast.body.id };
         let node: Flat_Let        = { id: index, token: nested_ast.token, kind: 'Flat_Let', binding: binding_id, value: value_id, body: body_id };
         flat_ast[index]           = node;
-        flat_ast                  = flatten_nodes(nested_ast.data[1], flat_ast);
-        flat_ast                  = flatten_nodes(nested_ast.data[2], flat_ast);
-        flat_ast                  = flatten_nodes(nested_ast.data[3], flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.binding, flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.value, flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.body, flat_ast);
     }
     else if (is_call(nested_ast)) {
         const index               = nested_ast.id;
-        const body_id             = { id: nested_ast.data[0].id };
-        const arg_id              = { id: nested_ast.data[1].id };
+        const body_id             = { id: nested_ast.fn.id };
+        const arg_id              = { id: nested_ast.arg.id };
         let node: Flat_Call       = { id: index, token: nested_ast.token, kind: 'Flat_Call', body: body_id, arg: arg_id };
         flat_ast[index]           = node;
-        flat_ast                  = flatten_nodes(nested_ast.data[0], flat_ast);
-        flat_ast                  = flatten_nodes(nested_ast.data[1], flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.fn, flat_ast);
+        flat_ast                  = flatten_nodes(nested_ast.arg, flat_ast);
     }
     else if (is_boolean(nested_ast) || is_number(nested_ast) || is_string(nested_ast)) {
         const index               = nested_ast.id;
@@ -45,7 +45,7 @@ function flatten_nodes(nested_ast: AST, flat_ast: Flat_AST): Flat_AST {
     }
     else if (is_identifier(nested_ast)) {
         const index               = nested_ast.id;
-        let node: Flat_Identifier = { id: index, token: nested_ast.token, kind: 'Flat_Identifier', name: nested_ast.value };
+        let node: Flat_Identifier = { id: index, token: nested_ast.token, kind: 'Flat_Identifier', name: nested_ast.name };
         flat_ast[index]           = node;
     }
     else {
--- test/proto_flatten.test.ts
+++ test/proto_flatten.test.ts
@@ -1,138 +1,119 @@
 import { describe, it, expect } from 'vitest'
 import { lex } from '../src/lexer'
-import { is_error, is_ok, Result } from '../src/error';
-import { parse, parse_nested} from '../src/parser';
+import { parse } from '../proto/parser_oo';
 import { Value, Id, Flat_Constant, Flat_Identifier, Flat_Reference, Flat_Lambda, Flat_Let, Flat_Call, Flat_Plus, Flat_Minus, Flat_Node, Flat_Atom, Flat_AST } from "./../proto/flat_ast";
 import { flatten } from '../proto/flatten';
-
-import { Token } from '../src/token';
-import { AST } from '../src/ast';
-
-type OkLex       = { ok: true, value: readonly Token[] };
-type OkParse     = { ok: true, value: AST };
-type NestedParse = { node_counter: number, result: Result<AST> };
 
 describe('convert atoms', () => {
 
     it('must parse "true" to a boolean', () => {
-        const lexed = lex('true');
-        expect(is_ok(lexed)).toBe(true);
-        const parsed: NestedParse = parse_nested((lexed as OkLex).value);
-        expect(is_ok(parsed.result)).toBe(true);
-        const ast: AST = (parsed.result as OkParse).value;
-        const flat_ast: Flat_AST = flatten(ast, parsed.node_counter);
+        const text: string       = 'true';
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
-        expect(flat_ast[0].kind).toBe('Flat_Constant');
-        expect(flat_ast[0].token).toBe(0);
-        expect((flat_ast[0] as Flat_Constant).value).toBe(true);
+        const expected: Flat_Node[] = [
+            {id: 0, token: 0, kind: 'Flat_Constant', value: true},
+        ];
+
+        expect(node_count).toBe(1);
+        expect(flat_ast.length).toBe(1);
+        expect(flat_ast).toStrictEqual(expected);
     });
 
     it('must parse "-0.1" to a number'), () => {
-        const lexed = lex('-0.1');
-        expect(is_ok(lexed)).toBe(true);
-        const parsed: NestedParse = parse_nested((lexed as OkLex).value);
-        expect(is_ok(parsed.result)).toBe(true);
-        const ast: AST = (parsed.result as OkParse).value;
-        const flat_ast: Flat_AST = flatten(ast, parsed.node_counter);
+        const text: string       = '-0.1';
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
-        expect(flat_ast[0].kind).toBe('Flat_Constant');
-        expect(flat_ast[0].token).toBe(0);
-        expect((flat_ast[0] as Flat_Constant).value).toBe(-0.1);
+        const expected: Flat_Node[] = [
+            {id: 0, token: 0, kind: 'Flat_Constant', value: -0.1},
+        ];
+        expect(node_count).toBe(1);
+        expect(flat_ast.length).toBe(1);
+        expect(flat_ast).toStrictEqual(expected);
     }
 });
 
-describe.skip('expressions', () => {
+describe('expressions', () => {
     it('must produce a valid AST for arithemetic expressions', () => {
-        const lexed = lex("((+ 1) 2)");
-        expect(is_ok(lexed)).toBe(true);
-        const parsed: NestedParse = parse_nested((lexed as OkLex).value);
-        expect(is_ok(parsed.result)).toBe(true);
-        const ast: AST = (parsed.result as OkParse).value;
-        const flat_ast: Flat_AST = flatten(ast, parsed.node_counter);
+        const text: string       = "((+ 1) 2)";
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
+        const expected: Flat_Node[] = [
+            {id: 0, token: 0, kind: 'Flat_Call', body: {id: 1}, arg: {id: 4}},
+            {id: 1, token: 1, kind: 'Flat_Call', body: {id: 2}, arg: {id: 3}},
+            {id: 2, token: 2, kind: 'Flat_Identifier', name: '+'},
+            {id: 3, token: 4, kind: 'Flat_Constant', value: 1},
+            {id: 4, token: 7, kind: 'Flat_Constant', value: 2},
+        ];
+        
+        expect(node_count).toBe(5);
         expect(flat_ast.length).toBe(5);
-        expect(flat_ast[0].kind).toBe('Flat_Call');
-        expect((flat_ast[0] as Flat_Call).arg.id).toBe(4);
+        expect(flat_ast).toStrictEqual(expected);
+    });
 
-        expect(flat_ast[1].kind).toBe('Flat_Call');
-        expect((flat_ast[1] as Flat_Call).arg.id).toBe(3);
+    it('must produce a valid AST for a simple lambda expression', () => {
+        const text: string       = "((lambda x x) 42)";
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
-        expect(flat_ast[2].kind).toBe('Flat_Identifier');
-        expect((flat_ast[2] as Flat_Identifier).name).toStrictEqual('+');
-
-        expect(flat_ast[3].kind).toBe('Flat_Constant');
-        expect((flat_ast[3] as Flat_Constant).value).toBe(1);
-
-        expect(flat_ast[4].kind).toBe('Flat_Constant');
-        expect((flat_ast[4] as Flat_Constant).value).toBe(2);
+        const expected: Flat_Node[] = [
+            {id: 0, token: 0, kind: 'Flat_Call', body: {id: 1}, arg: {id: 4}},
+            {id: 1, token: 1, kind: 'Flat_Lambda', binding: {id: 2}, body: {id: 3}},
+            {id: 2, token: 4, kind: 'Flat_Identifier', name: 'x'},
+            {id: 3, token: 6, kind: 'Flat_Identifier', name: 'x'},
+            {id: 4, token: 9, kind: 'Flat_Constant', value: 42},
+        ];
+        
+        expect(node_count).toBe(5);
+        expect(flat_ast.length).toBe(5);
+        expect(flat_ast).toStrictEqual(expected);
     });
 
     it('must produce a valid AST for lambda expressions', () => {
-        const lexed = lex("(((lambda a (lambda b a)) 1) 2)");
-        expect(is_ok(lexed)).toBe(true);
-        const parsed: NestedParse = parse_nested((lexed as OkLex).value);
-        expect(is_ok(parsed.result)).toBe(true);
-        const ast: AST = (parsed.result as OkParse).value;
-        const flat_ast: Flat_AST = flatten(ast, parsed.node_counter);
+        const text: string       = "(((lambda a (lambda b a)) 1) 2)";
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
         const expected: Flat_Node[] = [
-            {id: 0, token: 0, kind: 'Flat_Call', body: {id: 1}, arg: {id: 8}},
-            {id: 1, token: 1, kind: 'Flat_Call', body: {id: 2}, arg: {id: 7}},
-            {id: 2, token: 2, kind: 'Flat_Lambda', binding: {id: 3}, body: {id: 4}},
-            {id: 3, token: 6, kind: 'Flat_Identifier', name: 'a'},
-            {id: 4, token: 8, kind: 'Flat_Lambda', binding: {id: 5}, body: {id: 6}},
-            {id: 5, token: 11, kind: 'Flat_Identifier', name: 'b'},
-            {id: 6, token: 13, kind: 'Flat_Reference', target: {id: 3}},
-            {id: 7, token: 17, kind: 'Flat_Constant', value: 1},
-            {id: 8, token: 20, kind: 'Flat_Constant', value: 2}
+            {id: 0, token:  0, kind: 'Flat_Call', body: {id: 1}, arg: {id: 8}},
+            {id: 1, token:  1, kind: 'Flat_Call', body: {id: 2}, arg: {id: 7}},
+            {id: 2, token:  2, kind: 'Flat_Lambda', binding: {id: 3}, body: {id: 4}},
+            {id: 3, token:  5, kind: 'Flat_Identifier', name: 'a'},
+            {id: 4, token:  7, kind: 'Flat_Lambda', binding: {id: 5}, body: {id: 6}},
+            {id: 5, token: 10, kind: 'Flat_Identifier', name: 'b'},
+            {id: 6, token: 12, kind: 'Flat_Identifier', name: 'a'},
+            {id: 7, token: 16, kind: 'Flat_Constant', value: 1},
+            {id: 8, token: 19, kind: 'Flat_Constant', value: 2}
         ];
 
+        expect(node_count).toBe(9);
         expect(flat_ast.length).toBe(9);
-        expect(flat_ast[0].kind).toBe('Flat_Call');
-        expect((flat_ast[0] as Flat_Call).arg.id).toBe(4);
-
-        expect(flat_ast[1].kind).toBe('Flat_Call');
-        expect((flat_ast[1] as Flat_Call).arg.id).toBe(3);
-
-        expect(flat_ast[2].kind).toBe('Flat_Lambda');
-        expect(flat_ast[3].kind).toBe('Flat_Identifier');
-        expect(flat_ast[4].kind).toBe('Flat_Lambda');
-        expect(flat_ast[5].kind).toBe('Flat_Identifier');
-        expect(flat_ast[6].kind).toBe('Flat_Reference');
-
-        expect(flat_ast[7].kind).toBe('Flat_Constant');
-        expect((flat_ast[7] as Flat_Constant).value).toBe(1);
-
-        expect(flat_ast[8].kind).toBe('Flat_Constant');
-        expect((flat_ast[8] as Flat_Constant).value).toBe(2);
+        expect(flat_ast).toStrictEqual(expected);
     });
 
     it('must produce a valid AST for let-bindings', () => {
         // (let x 42 x)
-        const lexed = lex("(let x x 42)");
-        expect(is_ok(lexed)).toBe(true);
-        const parsed: NestedParse = parse_nested((lexed as OkLex).value);
-        expect(is_ok(parsed.result)).toBe(true);
-        const ast: AST = (parsed.result as OkParse).value;
-        const flat_ast: Flat_AST = flatten(ast, parsed.node_counter);
+        const text: string       = "(let x 42 x)";
+        const parsed             = parse(lex(text));
+        const node_count: number = parsed.node_count;
+        const flat_ast: Flat_AST = flatten(parsed.ast, parsed.node_count);
 
         const expected: Flat_Node[] = [
             {id: 0, token: 0, kind: 'Flat_Let', binding: {id: 1}, value: {id: 2}, body: {id: 3}},
             {id: 1, token: 3, kind: 'Flat_Identifier', name: 'x'},
             {id: 2, token: 5, kind: 'Flat_Constant', value: 42},
-            {id: 3, token: 7, kind: 'Flat_Reference', target: {id: 1}},
+            {id: 3, token: 7, kind: 'Flat_Identifier', name: 'x'},
         ];
 
+        expect(node_count).toBe(4);
         expect(flat_ast.length).toBe(4);
-        expect(flat_ast[0].kind).toBe('Flat_Let');
-
-        expect(flat_ast[1].kind).toBe('Flat_Identifier');
-        expect((flat_ast[1] as Flat_Identifier).name).toBe('x');
-
-        expect(flat_ast[2].kind).toBe('Flat_Constant');
-        expect((flat_ast[2] as Flat_Constant).value).toBe('42');
-
-        expect(flat_ast[3].kind).toBe('Flat_Reference');
-        expect((flat_ast[3] as Flat_Reference).target.id).toBe(1);
+        expect(flat_ast).toStrictEqual(expected);
     });
 });
