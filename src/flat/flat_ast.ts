67e422100f111f769cb35afd039407fb5ced7a05
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-13 10:06:37 +0200

    make token id optional in the flat AST, easier for handwritten testcases

--- src/flat/flat_ast.ts
+++ src/flat/flat_ast.ts
@@ -2,14 +2,14 @@
 
 export type Value           = boolean | number;
 export type Id              = {id: number};
-export type Flat_Literal    = {id: number, token: number, kind: 'Flat_Literal', value: (boolean | number | string)};
-export type Flat_Identifier = {id: number, token: number, kind: 'Flat_Identifier', name: string};
-export type Flat_Reference  = {id: number, token: number, kind: 'Flat_Reference', target: Id};
-export type Flat_Lambda     = {id: number, token: number, kind: 'Flat_Lambda', binding: Id, body: Id};
-export type Flat_Let        = {id: number, token: number, kind: 'Flat_Let', binding: Id, value: Id, body: Id};
-export type Flat_Call       = {id: number, token: number, kind: 'Flat_Call', body: Id, arg: Id};
-export type Flat_Plus       = {id: number, token: number, kind: 'Flat_Plus'};
-export type Flat_Minus      = {id: number, token: number, kind: 'Flat_Minus'};
+export type Flat_Literal    = {id: number, token?: number, kind: 'Flat_Literal', value: (boolean | number | string)};
+export type Flat_Identifier = {id: number, token?: number, kind: 'Flat_Identifier', name: string};
+export type Flat_Reference  = {id: number, token?: number, kind: 'Flat_Reference', target: Id};
+export type Flat_Lambda     = {id: number, token?: number, kind: 'Flat_Lambda', binding: Id, body: Id};
+export type Flat_Let        = {id: number, token?: number, kind: 'Flat_Let', binding: Id, value: Id, body: Id};
+export type Flat_Call       = {id: number, token?: number, kind: 'Flat_Call', body: Id, arg: Id};
+export type Flat_Plus       = {id: number, token?: number, kind: 'Flat_Plus'};
+export type Flat_Minus      = {id: number, token?: number, kind: 'Flat_Minus'};
 export type Flat_Node       = Flat_Literal | Flat_Identifier | Flat_Reference | Flat_Lambda | Flat_Let | Flat_Call | Flat_Plus | Flat_Minus;
 export type Flat_Atom       = Flat_Literal | Flat_Identifier;
 export type Flat_AST        = Flat_Node[];
