0bfd057a3c05e5b15361710b1e033b552e495409
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-19 17:22:29 +0200

    rename 'Environment' to 'Scope' in the reference resolution

--- src/flat/reference_resolution.ts
+++ src/flat/reference_resolution.ts
@@ -2,23 +2,23 @@
 
 import { Flat_Binding, Flat_Reference, Flat_Expression, Flat_AST, is_literal, is_identifier, is_reference, is_lambda, is_let, is_call, is_plus, is_minus, is_binding } from "./flat_ast";
 
-export type LookupTable = {
-    parent: undefined | LookupTable,
+export type Scope = {
+    parent: undefined | Scope,
     bindings: Map<string, number>,
 };
 
-export function make_env(): LookupTable { return { parent: undefined, bindings: new Map<string, number>()} }
+export function make_scope(): Scope { return { parent: undefined, bindings: new Map<string, number>()} }
 
-function extend_env(env: LookupTable): LookupTable { return { parent: env, bindings: new Map<string, number>()}; }
+function extend_scope(scope: Scope): Scope { return { parent: scope, bindings: new Map<string, number>()}; }
 
-export function lookup(name: string, env: LookupTable): number {
-    const entry: undefined | number = env.bindings.get(name);
+export function lookup(name: string, scope: Scope): number {
+    const entry: undefined | number = scope.bindings.get(name);
     if (entry !== undefined) {
         return entry;
     }
     else {
-        if (env.parent !== undefined) {
-            return lookup(name, env.parent);
+        if (scope.parent !== undefined) {
+            return lookup(name, scope.parent);
         }
         else {
             throw new Error(`variable with name ${name} is undefined`)
@@ -28,19 +28,19 @@ export function lookup(name: string, env: LookupTable): number {
 
 export function resolve_references(ast: Flat_AST): Flat_AST {
     let copy = ast.map(x => x);
-    let env = make_env();
+    let scope = make_scope();
 
-    return resolve(copy[0], copy, env);
+    return resolve(copy[0], copy, scope);
 }
 
-function resolve(expr: Flat_Expression, ast: Flat_AST, env: LookupTable): Flat_AST {
+function resolve(expr: Flat_Expression, ast: Flat_AST, scope: Scope): Flat_AST {
     if (is_literal(expr, ast) || is_binding(expr, ast) || is_reference(expr, ast)) {
         return ast;
     }
     else if (is_identifier(expr, ast)) {
         const current_id: number                = expr.id;
         const current_token: undefined | number = expr.token;
-        const target_id: number                 = lookup(expr.name, env);
+        const target_id: number                 = lookup(expr.name, scope);
         const ref: Flat_Reference               = { id: current_id, token: current_token, kind: "Flat_Reference", target: {id: target_id} };
         ast[current_id]                         = ref;
         return ast;
@@ -49,10 +49,10 @@ function resolve(expr: Flat_Expression, ast: Flat_AST, env: LookupTable): Flat_A
         const binding_node: Flat_Binding = ast[expr.binding.id] as Flat_Binding;
         const body_node: Flat_Expression = ast[expr.body.id];
 
-        let extended_env = extend_env(env);
-        extended_env.bindings.set(binding_node.name, binding_node.id);
+        let extended_scope = extend_scope(scope);
+        extended_scope.bindings.set(binding_node.name, binding_node.id);
 
-        ast = resolve(body_node, ast, extended_env);
+        ast = resolve(body_node, ast, extended_scope);
         return ast;
     }
     else if (is_let(expr, ast)) {
@@ -60,19 +60,19 @@ function resolve(expr: Flat_Expression, ast: Flat_AST, env: LookupTable): Flat_A
         const value_node: Flat_Expression = ast[expr.value.id];
         const body_node: Flat_Expression  = ast[expr.body.id];
 
-        let extended_env = extend_env(env);
-        extended_env.bindings.set(binding_node.name, binding_node.id);
+        let extended_scope = extend_scope(scope);
+        extended_scope.bindings.set(binding_node.name, binding_node.id);
 
-        ast = resolve(value_node, ast, env); // the value is evaluated with the original env
-        ast = resolve(body_node, ast, extended_env);
+        ast = resolve(value_node, ast, scope); // the value is evaluated with the original scope
+        ast = resolve(body_node, ast, extended_scope);
         return ast;
     }
     else if (is_call(expr, ast)) {
         const fn_node  = ast[expr.body.id];
         const arg_node = ast[expr.arg.id];
 
-        ast = resolve(fn_node, ast, env);
-        ast = resolve(arg_node, ast, env);
+        ast = resolve(fn_node, ast, scope);
+        ast = resolve(arg_node, ast, scope);
         return ast;
     }
     else {
