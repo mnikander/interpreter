61e5d5b3a571890690c2fc92979ba6f9690779d6
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-19 17:49:39 +0200

    create stubs for the resolution of built-in functions

--- src/flat/reference_resolution.ts
+++ src/flat/reference_resolution.ts
@@ -1,48 +1,71 @@
 // Copyright (c) 2025 Marco Nikander
 
+import { Item } from "../item";
 import { Flat_Binding, Flat_Reference, Flat_Expression, Flat_AST, is_literal, is_identifier, is_reference, is_lambda, is_let, is_call, is_plus, is_minus, is_binding } from "./flat_ast";
 
+export type GlobalScope = {
+    kind: "GlobalScope"
+    bindings: Map<string, ("builtin" | number)>;
+};
+
 export type Scope = {
-    parent: undefined | Scope,
+    kind: "Scope"
+    parent: Scope | GlobalScope,
     bindings: Map<string, number>,
 };
 
-export function make_scope(): Scope { return { parent: undefined, bindings: new Map<string, number>()} }
+export function is_global_scope(item: Item): item is GlobalScope { return item.kind === "GlobalScope"; }
+export function is_local_scope(item: Item): item is Scope { return item.kind === "Scope"; }
+export function make_global_scope(): GlobalScope { return { kind: "GlobalScope", bindings: new Map<string, "builtin" | number>()} }
+function extend_scope(scope: GlobalScope | Scope): Scope { return { kind: "Scope", parent: scope, bindings: new Map<string, number>()}; }
 
-function extend_scope(scope: Scope): Scope { return { parent: scope, bindings: new Map<string, number>()}; }
-
-export function lookup(name: string, scope: Scope): number {
-    const entry: undefined | number = scope.bindings.get(name);
-    if (entry !== undefined) {
-        return entry;
-    }
-    else {
-        if (scope.parent !== undefined) {
-            return lookup(name, scope.parent);
+export function lookup(name: string, scope: GlobalScope | Scope): "builtin" | number {
+    if (is_global_scope(scope)) {
+        const entry: undefined | number | "builtin" = scope.bindings.get(name);
+        if (entry !== undefined) {
+            return entry;
         }
         else {
             throw new Error(`variable with name ${name} is undefined`)
         }
     }
+    else if (is_local_scope(scope)) {
+        const entry: undefined | number = scope.bindings.get(name);
+        if (entry !== undefined) {
+            return entry;
+        }
+        else {
+            return lookup(name, scope.parent);
+        }
+    }
+    else {
+        throw Error('Invalid kind of scope. Something is wrong with the reference resolution implementation.')
+    }
 }
 
 export function resolve_references(ast: Flat_AST): Flat_AST {
     let copy = ast.map(x => x);
-    let scope = make_scope();
+    let scope = make_global_scope();
 
     return resolve(copy[0], copy, scope);
 }
 
-function resolve(expr: Flat_Expression, ast: Flat_AST, scope: Scope): Flat_AST {
+function resolve(expr: Flat_Expression, ast: Flat_AST, scope: GlobalScope | Scope): Flat_AST {
     if (is_literal(expr, ast) || is_binding(expr, ast) || is_reference(expr, ast)) {
         return ast;
     }
     else if (is_identifier(expr, ast)) {
         const current_id: number                = expr.id;
         const current_token: undefined | number = expr.token;
-        const target_id: number                 = lookup(expr.name, scope);
-        const ref: Flat_Reference               = { id: current_id, token: current_token, kind: "Flat_Reference", target: {id: target_id} };
-        ast[current_id]                         = ref;
+        const target_id: "builtin" | number     = lookup(expr.name, scope);
+        if (target_id === "builtin") {
+            // TODO: create an AST node for built-in functions
+            throw Error('Resolution of builtin functions has not been implemented yet.')
+        }
+        else {
+            const ref: Flat_Reference = { id: current_id, token: current_token, kind: "Flat_Reference", target: {id: target_id} };
+            ast[current_id]           = ref;
+        }
         return ast;
     }
     else if (is_lambda(expr, ast)) {
