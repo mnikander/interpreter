f2778fbce62ca7f015873db296d601d2236ac17c
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-20 09:41:26 +0200

    add help menu to the HTML page, so it does not need to be part of the language

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,22 @@ function run_interpreter() {
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Help menu logic (toggle with one button)
+    const helpBtn = document.getElementById("help-btn");
+    const helpBox = document.getElementById("help-box");
+
+    helpBtn?.addEventListener("click", () => {
+        if (helpBox && helpBtn) {
+            if (helpBox.style.display === "block") {
+                helpBox.style.display = "none";
+                helpBtn.textContent = "Help";
+            } else {
+                helpBox.style.display = "block";
+                helpBtn.textContent = "Close Help";
+            }
+        }
+    });
+
     const output = document.getElementById("output");
     if (output) {
         output.textContent = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome! Try typing:\n\n(+ 1 2)\n\nand press Enter. For more info run:\n\n(help)\n\n";
