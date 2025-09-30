8360c989456fce21659ae16779b90c51582be21a
Marco Nikander <m4n1k.developer@gmail.com>
2025-09-30 21:15:37 +0200

    restore old code for checking parentheses

---
+++ src/parentheses.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2025 Marco Nikander
+
+export function check_parentheses(line: string): boolean {
+    let count = 0;
+    let open_before_close = true;
+
+    for (let i = 0; i < line.length; i++) {
+        if (line[i] == '(') {
+            count++;
+        }
+        else if (line[i] == ')') {
+            count--;
+        }
+        if (count < 0) {
+            open_before_close = false;
+        }
+    }
+
+    let balanced = (count == 0);
+
+    return open_before_close && balanced;
+}
+
+export function add_whitespace_to_parentheses(line: string): string {
+    let output = "";
+    for (let i = 0; i < line.length; i++) {
+        if (line[i] == '(') {
+            output += ' ( ';
+        }
+        else if(line[i] == ')') {
+            output += ' ) ';
+        }
+        else {
+            output += line[i];
+        }
+    }
+    return output;
+}
