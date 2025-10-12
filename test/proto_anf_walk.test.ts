c204641d13024dfa60d3ffff68005eac2d5ad86b
Marco Nikander <m4n1k.developer@gmail.com>
2025-10-12 18:08:11 +0200

    test pre-order traversal of a nested function call

--- test/proto_anf_walk.test.ts
+++ test/proto_anf_walk.test.ts
@@ -31,4 +31,20 @@ describe('in-place editing', () => {
         expect(parsed.ast.body.fn.id).toBe(100); // !
         expect(parsed.ast.body.arg.id).toBe(101); // false
     });
+
+    it('must be able to traverse nested expressions', () => {
+        const text: string = "((+ 1) 2)";
+        const parsed = parse(lex(text));
+
+        let counter = {index: 100};
+        const visitor = { pre: (node: _Expression, context: {index: number}) => { node.id = context.index; context.index++;} };
+        const answer = walk(parsed.ast, visitor, counter);
+        expect(parsed.ast.id).toBe(100); // block
+        expect(parsed.ast.body.id).toBe(101); // call
+        expect(parsed.ast.body.fn.id).toBe(102); // block
+        expect(parsed.ast.body.fn.body.id).toBe(103); // call
+        expect(parsed.ast.body.fn.body.fn.id).toBe(104); // +
+        expect(parsed.ast.body.fn.body.arg.id).toBe(105); // 1
+        expect(parsed.ast.body.arg.id).toBe(106); // 2
+    });
 });
